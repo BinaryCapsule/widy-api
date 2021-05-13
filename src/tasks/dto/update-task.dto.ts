55821984b867114f69e1562111f1ad58f53e2f2d
Jose Nunes <josenunes34@gmail.com>
2021-05-13 23:23:14 +0100

    wip

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,6 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTaskDto } from './create-task.dto';
-import { IsBoolean, IsOptional } from 'class-validator';
+import { IsBoolean, IsNumber, IsOptional } from 'class-validator';
 
 export class UpdateTaskDto extends PartialType(CreateTaskDto) {
   @IsBoolean()
@@ -13,4 +13,8 @@ export class UpdateTaskDto extends PartialType(CreateTaskDto) {
   @IsBoolean()
   @IsOptional()
   readonly isSchedule?: boolean;
+
+  @IsNumber()
+  @IsOptional()
+  readonly time?: number;
 }
