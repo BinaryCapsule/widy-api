6d28da223aef8f22ca5a4ba8f23d0d3990cd0072
Jose Nunes <josenunes34@gmail.com>
2021-10-09 11:16:39 +0100

    feat: tomorrow section

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -16,6 +16,8 @@ import { TaskQueryDto } from './dto/task-query.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from '../auth/get-user.decorator';
 import { User } from '../auth/User';
+import { MoveToPlanDto } from './dto/move-to-plan.dto';
+import { MoveAllToPlanDto } from './dto/move-all-to-plan.dto';
 
 @UseGuards(AuthGuard('jwt'))
 @Controller('tasks')
@@ -34,6 +36,11 @@ export class TasksController {
     return { id: task ? task.id : null };
   }
 
+  @Patch('/to-plan')
+  async moveAllToPlan(@Body() moveAllToPlanDto: MoveAllToPlanDto, @GetUser() user: User) {
+    return this.tasksService.moveAllToPlan(moveAllToPlanDto, user.sub);
+  }
+
   @Get(':id')
   findOne(@Param('id') id: number, @GetUser() user: User) {
     return this.tasksService.findOne(id, user.sub);
@@ -47,6 +54,11 @@ export class TasksController {
   @Patch('/:id/to-tomorrow')
   moveToTomorrow(@Param('id') id: number, @GetUser() user: User) {
     return this.tasksService.moveToTomorrow(id, user.sub);
+  }
+
+  @Patch('/:id/to-plan')
+  moveToPlan(@Param('id') id: number, @Body() moveToPlanDto: MoveToPlanDto, @GetUser() user: User) {
+    return this.tasksService.moveToPlan(id, moveToPlanDto, user.sub);
   }
 
   @Patch(':id')
