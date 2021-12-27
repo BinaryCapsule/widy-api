529b4779eaf394e66ba1b8d803154773baae076e
Jose Nunes <jose.nunes@orion.com>
2021-12-27 17:53:37 +0000

    feat(DaysService): migrate to prisma

--- src/days/days.module.ts
+++ src/days/days.module.ts
@@ -5,10 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Section } from 'src/sections/entities/section.entity';
 import { Task } from '../tasks/entities/task.entity';
 import { Day } from './enities/day.entity';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Day, Section, Task])],
   controllers: [DaysController],
-  providers: [DaysService],
+  providers: [DaysService, PrismaService],
 })
 export class DaysModule {}
